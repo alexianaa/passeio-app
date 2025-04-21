27d815180110d7959cbc54448156545af8a368f9
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-04-21 08:28:48 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./template/template.module').then(module => module.TemplateModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
