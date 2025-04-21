799446d593cd5219563280ef1dba48f3330d94ad
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-04-21 12:20:49 -0300

    muda tituto e subtitulo de acordo com pagina atual

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -4,9 +4,24 @@ import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent, children: [
-    {path: 'categorias', loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule), pathMatch: 'full'},
-    {path: 'lugares', loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule), pathMatch: 'full'},
-    {path: 'galeria', loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule), pathMatch: 'full'},
+    {
+      path: 'categorias', 
+      loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule), 
+      pathMatch: 'full',
+      data: { titulo: 'Categorias', subtitulo: 'Realize o cadastro de novas categorias.' }
+    },
+    {
+      path: 'lugares', 
+      loadChildren: () => import('../lugares/lugares.module').then(m => m.LugaresModule), 
+      pathMatch: 'full',
+      data: { titulo: 'Lugares', subtitulo: 'Realize o cadastro de novos lugares.' }
+    },
+    {
+      path: 'galeria', 
+      loadChildren: () => import('../galeria/galeria.module').then(m => m.GaleriaModule), 
+      pathMatch: 'full',
+      data: { titulo: 'Galeria', subtitulo: 'Descubra os melhores lugares para ir se divertir.' }
+    },
   ]}
 ];
 
