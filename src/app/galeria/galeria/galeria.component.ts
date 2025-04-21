7185ce15597cb449d27e04b370d219941593ddb9
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-04-21 12:03:53 -0300

    adiciona filtro de lugares

--- src/app/galeria/galeria/galeria.component.ts
+++ src/app/galeria/galeria/galeria.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { GaleriaService } from '../galeria.service';
 import { Lugar } from '../../lugares/lugar';
 import { Categoria } from '../../categorias/categoria';
 import { LugarService } from '../../lugares/lugar.service';
@@ -14,6 +13,8 @@ import { CategoriaService } from '../../categorias/categoria.service';
 export class GaleriaComponent implements OnInit {
   lugares: Lugar[] = [];
   categoriasFiltro: Categoria[] = [];
+  nomeFiltro: string = '';
+  categoriaFiltro: string = '';
 
   constructor(
     private lugarService: LugarService,
@@ -29,6 +30,11 @@ export class GaleriaComponent implements OnInit {
 
   getTotalEstrelas(lugar: Lugar): string {
     return '&#9733;'.repeat(lugar.avaliacao || 0) + '&#9734;'.repeat(5 - (lugar.avaliacao || 0));
+  }
+
+  filtrar(){
+    this.lugarService.filtrar(this.nomeFiltro, this.categoriaFiltro)
+      .subscribe(resultado => this.lugares = resultado);
   }
 
 }
